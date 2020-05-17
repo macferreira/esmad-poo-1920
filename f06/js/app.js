85a0ad127120d706e7da5326936215876ff1ab03
Miguel Ferreira <miguel.correiaferreira@gmail.com>
2020-05-17 16:54:29 +0100

    Refactor routes handling and data fixtures import

--- f06/js/app.js
+++ f06/js/app.js
@@ -1,20 +1,24 @@
-import Routes from './routes.js'
+import UserView from './views/UserView.js';
+import CatalogView from './views/CatalogView.js';
+import BandDetailView from './views/BandDetailView.js'
 
 class App {
     constructor() {
-        this.routes = new Routes();
-        this._instantiateViews()
+        this.routes = {
+            'index': [
+                UserView,
+                CatalogView
+            ],
+            'band': [
+                BandDetailView
+            ]
+        };
 
-        // To Be Removed: Temporarly adding dummy data
-        if (!localStorage.bands) {
-            let bands = [];
-            const band1 = { id: 1, name: 'Muse', genre: 'Pop-Rock', photo: 'http://www.planckmachine.com/wp-content/uploads/2016/09/hysteria-muse-meaning-song.jpg', description: 'The best band ever', video: 'xxx' }
-            const band2 = { id: 2, name: 'RadioHead', genre: 'Pop-Rock', photo: 'https://ep01.epimg.net/elpais/imagenes/2017/05/17/icon/1495017818_647155_1495125183_noticia_normal.jpg', description: 'The best band ever', video: 'xxx' }
-            const band3 = { id: 3, name: 'James', genre: 'Pop-Rock', photo: 'http://ksassets.timeincuk.net/wp/uploads/sites/55/2013/01/2012JamesBandPress181212-2.jpg', decription: 'The best band ever', video: 'xxx' }
-            const band4 = { id: 4, name: 'Metallica', genre: 'Metal', photo: 'https://images.impresa.pt/blitz/2016-08-19-metallica.jpg/original/mw-860', description: 'The best band ever', video: 'xxx' }
-            bands.push(band1, band2, band3, band4);
-            localStorage.setItem("bands", JSON.stringify(bands));
-        }
+        // import dummy data for testing purposes
+        this._importDataFixtures();
+
+        // instantiate the views mapped in the routes object
+        this._instantiateViews();
     }
 
     _instantiateViews() {
@@ -22,12 +26,69 @@ class App {
         const file = path.substr(path.lastIndexOf('/') + 1);
         const route = file.split('.')[0];
 
-        const views = this.routes.getViews(route);
+        const views = this._getViews(route);
 
         for (const view of views) {
             new view();
         }
     }
+
+    _getViews(route) {
+        return typeof this.routes[route] === 'undefined' ? [] : this.routes[route];
+    }
+
+    _importDataFixtures() {
+        const bands = [
+            {
+                id: 1,
+                name: 'Muse',
+                genre: 'Pop-Rock',
+                photo: 'http://www.planckmachine.com/wp-content/uploads/2016/09/hysteria-muse-meaning-song.jpg',
+                description: 'The best band ever',
+                video: 'https://www.youtube.com/watch?v=AR6A3dap6MI'  
+            },
+            {
+                id: 2,
+                name: 'RadioHead',
+                genre: 'Pop-Rock',
+                photo: 'https://ep01.epimg.net/elpais/imagenes/2017/05/17/icon/1495017818_647155_1495125183_noticia_normal.jpg',
+                description: 'The best band ever',
+                video: 'https://www.youtube.com/watch?v=fHiGbolFFGw'
+            },
+            {
+                id: 3,
+                name: 'James',
+                genre: 'Pop-Rock',
+                photo: 'http://ksassets.timeincuk.net/wp/uploads/sites/55/2013/01/2012JamesBandPress181212-2.jpg',
+                decription: 'The best band ever',
+                video: 'https://www.youtube.com/watch?v=BlucfrfxAUc'
+            },
+            {
+                id: 4,
+                name: 'Metallica',
+                genre: 'Metal',
+                photo: 'https://images.impresa.pt/blitz/2016-08-19-metallica.jpg/original/mw-860',
+                description: 'The best band ever',
+                video: 'https://www.youtube.com/watch?v=pZTJBViOoik' 
+            }
+        ];
+
+        const users = [
+            {
+                id: 1,
+                username: 'user',
+                password: 'user'
+            }
+        ];
+
+        // Load the fixtures in case there is no data in the local storage 
+        if (!localStorage.bands) {
+            localStorage.setItem('bands', JSON.stringify(bands));    
+        }
+        if (!localStorage.users) {
+            localStorage.setItem('users', JSON.stringify(users));
+        }
+    }
 }
 
-new App(); 
+new App();
